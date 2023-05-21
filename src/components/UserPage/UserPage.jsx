9fbf73639018b9c463759880c2d4165079d35433
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-05-21 11:48:50 -0400

    added placeholder cards on /user

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import { Button } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: '150px'
+  }));
+
   return (
     <div className="container">
       <h2>Overview</h2>
@@ -13,6 +30,83 @@ function UserPage() {
         <Button variant='outlined'>New Property</Button>
         <Button variant='outlined'>New Task</Button>
         <Button variant='outlined'>Assign Tasks</Button>
+      </div>
+      <div id='open-tasks'>
+        <h2>Open Tasks</h2>
+        <Grid container spacing={2}>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={3}>
+            <Card sx={{ width: 250, height: 150 }}>
+              <CardContent>
+                <Typography>
+                  123 Main St
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
