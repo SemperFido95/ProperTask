c00e2d6e44827c50f271b165a0363c64e51533a5
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-05-21 11:15:09 -0400

    added buttons to /user

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use('/api/user', userRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5019;
 
 /** Listen * */
 app.listen(PORT, () => {
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { Button } from '@mui/material';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <h2>Overview</h2>
+      <div id='home-buttons' style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <Button variant='outlined'>New Property</Button>
+        <Button variant='outlined'>New Task</Button>
+        <Button variant='outlined'>Assign Tasks</Button>
+      </div>
     </div>
   );
 }
