17b74000ac9b71c59ed7106ea824a48a8000dcc6
Bailey Fiedorowicz <bfiedorowicz1995@gmail.com>
2023-06-05 21:46:15 -0400

    some styling changes

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -42,6 +42,8 @@ function UserPage() {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    borderRadius: '1%',
+    border: 'none'
   };
 
   const handleCloseSnackbar = () => setSnackbar(null);
